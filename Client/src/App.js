bbdec9091dd9aad6d999ed4c74a066eecb932984
dhanesh1232 <dhaneshreddy980@gmail.com>
2025-01-03 19:56:29 +0530

    all things are removed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,8 @@
+import { Routes, Route } from "react-router-dom";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-import Header from "./components/Header";
-
 const App = () => {
-  return (
-    <div className="page-container fluid-container vh-100">
-      <Header />
-      <div className="container d-flex flex-column justify-content-center align-items-start">
-        <h1 className="">Welcome to our React App!</h1>
-        <p className="">
-          This is a simple example of a React component using Tailwind CSS.
-        </p>
-        <button className="btn btn-danger">Click me!</button>
-      </div>
-    </div>
-  );
+  return <h1>Hello ECOD</h1>;
 };
 export default App;
